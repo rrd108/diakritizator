1e60a1e6119f0cdd8ff37e89b9ac5eb76a887a47
rrd <rrd@1108.cc>
2021-04-07 13:31:44 +0200

    design update

--- index.php
+++ index.php
@@ -10,8 +10,8 @@ $latinBetuk = array('a', 'i', 'u', 'd', 'h', 'l', 'l', 'm', 'n', 'n', 'n', 'r', 
 							'A', 'I', 'U', 'D', 'H', 'L', 'L', 'M', 'N', 'N', 'N', 'R', 'R', 'S', 'S', 'T');
 
 try{
-   //$dbh = new PDO('mysql:host=localhost;dbname=diakritizalo', 'root', '');
-	$dbh = new PDO('mysql:host=localhost;dbname=reseller10_diak', 'reseller10_djzr', '5nU38NP6');
+  $dbh = new PDO('mysql:host=localhost;dbname=diakritizator', 'root', '123');
+	//$dbh = new PDO('mysql:host=localhost;dbname=reseller10_diak', 'reseller10_djzr', '5nU38NP6');
 	$dbh->query('SET NAMES "utf8"');
 
 	mb_internal_encoding('UTF-8');
@@ -55,72 +55,72 @@ catch(PDOException $e){
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-	<meta charset='utf-8' />
-	<title>Diakritizátor 2.0</title>
-	<link rel="stylesheet" media="all" href="diakritizator.css?v=130208"/>
-	<link rel="stylesheet" href="tooltipster.css" type="text/css" media="all" title="Screen">
-	<script src="jquery.js"></script>
-	<script src="jquery.tooltipster.js"></script>
-	<script src="diakritizator.js?v=130305"></script>
+  <meta charset='utf-8' />
+  <title>Diakritizátor 2.0</title>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" media="all" href="diakritizator.css?v=130208" />
+  <link rel="stylesheet" href="tooltipster.css" type="text/css" media="all" title="Screen">
+  <script src="jquery.js"></script>
+  <script src="jquery.tooltipster.js"></script>
+  <script src="diakritizator.js?v=130305"></script>
 </head>
+
 <body>
-<aside>
+  <header>
+    <h1>Diakritizátor</h1>
+    <img src="mkthk_logo.png" alt="logo">
+  </header>
 
-	<h2>Top 10</h2>
-	<ol>
-	<?php
-	try{
-		foreach($dbh->query('SELECT diakritikus FROM szoparok ORDER BY lekerve DESC, sima ASC LIMIT 10') as $row){
-			if($pipePos = strpos($row['diakritikus'],'|')){
-				$szo = substr($row['diakritikus'], 0, $pipePos);
-			}
-			else{
-				$szo = $row['diakritikus'];
-			}
-			print '<li>'.$szo.'</li>';
-		}
-		$dbh = null;
-	}
-	catch(PDOException $e){
-	   print "Error!: " . $e->getMessage() . "<br/>";
-	   die();
-	}
-	?>
-	</ol>
+  <main>
+    <section>
+      <h2>Átalakítandó szöveg</h2>
+      <p>Másolj be egy szöveget és megpróbáljuk beletenni a diakritikus jeleket.</p>
+      <form method="post" action="<?php print $_SERVER['PHP_SELF']; ?>">
+        <textarea id="forras" name="forras" rows="15" cols="75"><?php print $_REQUEST['forras']; ?></textarea>
+        <br>
+        <input type="submit" value="diaktritizálok">
+      </form>
+      <p contenteditable id="ujSzoveg"><?= nl2br($szoveg) ?></p>
+    </section>
 
-	<?php
-	if($szoveg){
-	?>
-		<a href="https://github.com/rrd108/diakritizator/issues">Hibajelentés</a>
-		<h2>Jelmagyarázat</h2>
-		<ul>
-			<li><span class="_c">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> cserélhető szó</li>
-		</ul>
-		<h3>Infók</h3>
-		<p>A szövegmezőbe bemásolt sima szöveget, vagy a régi Fólióból (BBtBooks) kimásolt szöveget alakítja diakritizált formára.</p>
-		<p>A <span class="_c">kiemelt hátterű</span> szavakra rámutatva meg lehet nézni a szó lehetséges variációit és rákattintva lehet váltogatni az állapotukat.</p>
-		<p>A nem kiemelt szavakra kattintva a teljes szöveg szerkeszthetővé válik.</p>
-		<h2>Figyelmeztetés</h2>
-		<p>Ez az alkalmazás <strong>NEM</strong> egy szanszkrit editori eszköz. Lehetnek <em>(és vannak is)</em> benne hibák, az átalakított szöveg nem feltétlenül helyes. Persze igyekszünk a <a href="https://github.com/rrd108/diakritizator/issues">visszajelzések</a> alapján javítani, fejleszteni.</p>
-	<?php
-	}
-	?>
-</aside>
-<section>
-	<form method="post" action="<?php print $_SERVER['PHP_SELF']; ?>">
-		Átalakítandó szöveg:<br>
-		<textarea
-			id="forras"
-			name="forras"
-			rows="15"
-			cols="75"><?php print $_REQUEST['forras']; ?></textarea>
-		<br>
-		<input type="submit" value="diaktritizálok">
-	</form>
-<?php
-print '<p contenteditable id="ujSzoveg">' . nl2br($szoveg) . '</p>';
-?>
-</section>
+    <aside>
+      <h2>Top 10</h2>
+      <ol>
+        <?php
+					try{
+						foreach($dbh->query('SELECT diakritikus FROM szoparok ORDER BY lekerve DESC, sima ASC LIMIT 10') as $row){
+							if($pipePos = strpos($row['diakritikus'],'|')){
+								$szo = substr($row['diakritikus'], 0, $pipePos);
+							}
+							else{
+								$szo = $row['diakritikus'];
+							}
+							print '<li>'.$szo.'</li>';
+						}
+						$dbh = null;
+					}
+					catch(PDOException $e){
+						print "Error!: " . $e->getMessage() . "<br/>";
+						die();
+					}
+				?>
+      </ol>
+
+      <h2>Infók</h2>
+      <p>A szövegmezőbe bemásolt sima szöveget, vagy a régi Fólióból (BBtBooks) kimásolt szöveget alakítja diakritizált
+        formára.</p>
+      <p>A <span class="_c">kiemelt hátterű</span> szavakra rámutatva meg lehet nézni a szó lehetséges variációit és
+        rákattintva lehet váltogatni az állapotukat.</p>
+      <p>A nem kiemelt szavakra kattintva a teljes szöveg szerkeszthetővé válik.</p>
+      <h2>Figyelmeztetés</h2>
+      <p>Ez az alkalmazás <strong>NEM</strong> egy szanszkrit editori eszköz. Lehetnek <em>(és vannak is)</em> benne
+        hibák, az átalakított szöveg nem feltétlenül helyes. Persze igyekszünk a <a
+          href="https://github.com/rrd108/diakritizator/issues">visszajelzések</a> alapján javítani, fejleszteni.</p>
+    </aside>
+  </main>
 </body>
+
 </html>
